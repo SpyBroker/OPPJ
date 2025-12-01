5c137e5a5762758e03273411a2305ecbed53afff
SpyBroker <aryamondal82@gmail.com>
2025-12-01 13:39:03 +0530

    commit

---
+++ sum.java
@@ -0,0 +1,8 @@
+public class sum {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int s = a + b;
+        System.out.println("The sum is: " + s);
+    }
+}
